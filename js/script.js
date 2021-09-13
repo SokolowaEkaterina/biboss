1d3effbf0eba81be82871d8e1ee3d1bd08bd50ce
SokolowaEkaterina <soko20wa.ek0ter1na@gmail.com>
2021-09-13 23:29:00 +0400

    adaptive

--- js/script.js
+++ js/script.js
@@ -9,4 +9,42 @@ $(document).ready(() => {
             $('.location_open_block').slideDown(500);
         }
     });
+
+    // $(window).resize(function() {
+    //     if (document.documentElement.clientWidth < 1420) {
+    //         $('.sections_title').on('click', function(e){
+    //             $(this).toggleClass('active');
+    //             $(this).next('ul').slideToggle(500);
+    //         });
+    //     }
+    // });
+
+    // var mediaQuery = window.matchMedia("screen and (width: 1420px)");
+    // mediaQuery.addListener(foo);
+    // foo(mediaQuery);
+
+    // function foo(mq) {
+    //     $('.sections_title').on('click', function(e){
+    //         $(this).toggleClass('active');
+    //         $(this).next('ul').slideToggle(500);
+    //     });
+    // }
+
+    $('.menu_button').on('click', function(e){
+        if($('.menu_button').hasClass('menu_active')){
+            $('.menu_button').removeClass('menu_active');
+            $('.navigation').removeClass('navigation_active');
+        }
+        else{
+            $('.menu_button').addClass('menu_active');
+            $('.navigation').addClass('navigation_active');
+        }
+    });
+
+
+    // $('.competitors_slider').slick({
+    //     slidesToShow: 2,
+    //     arrows: false,
+    //     centerMode: true,
+    // });
 });
